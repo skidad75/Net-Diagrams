3f14dac29fa55ec5c94462eea2b534dcff8164cf
Ryan S <skidad75@gmail.com>
2024-04-30 11:13:19 -0700

    sec31

--- netdiagrams.py
+++ netdiagrams.py
@@ -39,8 +39,9 @@ print(response.choices[0].message.content)
 print("Status Code:", response.status_code)
 print("Request Data:", data)
 print("Response Body:", response.text)
+
 try:
-    response_data = response.json()
+response_data = response.json()
     if 'choices' in response_data and response_data['choices']:
         return response_data['choices'][0]['text'].strip()
     else:
